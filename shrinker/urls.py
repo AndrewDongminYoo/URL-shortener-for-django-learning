47c0331d323d896c9604e5c3d4c3d78cdc7193d8
ydm2790 <ydm2790@gmail.com>
2022-01-15 02:07:40 +0900

    user register form

--- shrinker/urls.py
+++ shrinker/urls.py
@@ -12,7 +12,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from shortener.views import index, get_user
+from shortener.views import index, get_user, register
 from django.contrib import admin
 from django.urls import path, include
 
@@ -20,5 +20,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", index, name="index"),
     path("get_user/<int:user_id>", get_user),
+    path("register", register, name="register"),
     path('__debug__/', include('debug_toolbar.urls')),
 ]
