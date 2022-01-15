afe63394395d4113ce66177e86cf46876667e010
ydm2790 <ydm2790@gmail.com>
2022-01-16 01:05:30 +0900

    new model :: shortened urls create form

--- shortener/views.py
+++ shortener/views.py
@@ -1,12 +1,12 @@
-from django.contrib.auth.forms import AuthenticationForm, PasswordResetForm, PasswordChangeForm
 from django.http.response import JsonResponse
-from shortener.models import Users
-from django.shortcuts import redirect, render
 from django.views.decorators.csrf import csrf_exempt
-from shortener.forms import RegisterForm, LoginForm
-from django.contrib.auth import login, authenticate, logout
+from django.shortcuts import redirect, render
 from django.core.paginator import Paginator
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from shortener.models import Users, ShortenedUrls
+from shortener.forms import RegisterForm, LoginForm, UrlCreateForm
 
 # Create your views here.
 
@@ -95,4 +95,44 @@ def list_view(request):
 
 
 def url_list(request):
-    return render(request, "url_list.html")
+    url_array = ShortenedUrls.objects.order_by("-create_at").all()
+    return render(request, "url_list.html", {"list": url_array})
+
+
+@login_required
+def url_create(request):
+    msg = None
+    if request.method == "POST":
+        form = UrlCreateForm(request.POST)
+        if form.is_valid():
+            msg = f"{form.cleaned_data.get('nick_name')} 생성 완료!"
+            messages.add_message(request, messages.INFO, msg)
+            form.save_form(request)
+            return redirect("url_list")
+        else:
+            form = UrlCreateForm()
+    else:
+        form = UrlCreateForm()
+    return render(request, "url_create.html", {"msg": msg, "form": form})
+
+
+@login_required
+def url_change(request, action, url_id):
+    if request.method == "POST":
+        url_data = ShortenedUrls.objects.filter(id=url_id)
+        if url_data.exists():
+            if url_data.first().created_by_id == request.user.id:
+                if action == "delete":
+                    msg = f"{url_data.first().nick_name} 삭제 완료!"
+                    url_data.delete()
+                    messages.add_message(request, messages.INFO, msg)
+                elif action == "update":
+                    msg = f"{url_data.first().nick_name} 수정 완료!"
+                    form = UrlCreateForm(request.POST)
+                    form.update_form(request, url_id)
+                    messages.add_message(request, messages.INFO, msg)
+    elif request.method == "GET" and action == "update":
+        url_data = ShortenedUrls.objects.filter(pk=url_id).first()
+        form = UrlCreateForm(instance=url_data)
+        return render(request, "url_create.html", {"form": form, "is_update": True})
+    return redirect("url_list")
--- shrinker/urls.py
+++ shrinker/urls.py
@@ -9,7 +9,7 @@ Function views
 Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
+Including another URL conf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
@@ -17,17 +17,22 @@ import debug_toolbar
 from django.conf.urls import include
 from shortener.views import index, get_user
 from shortener.views import register, login_view, logout_view
-from shortener.views import url_list
+from shortener.views import url_list, url_create, url_change
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("__debug__/", include(debug_toolbar.urls)),  # Django Debug Tool
+
     path("", index, name="index"),
+
     path("register", register, name="register"),
     path("login", login_view, name="login"),
     path("logout", logout_view, name="logout"),
-    path("list", url_list, name="url_list"),
     path("get_user/<int:user_id>", get_user),
+
+    path("urls", url_list, name="url_list"),
+    path("urls/create", url_create, name="url_create"),
+    path("urls/<str:action>/<int:url_id>", url_change, name="url_change"),
 ]
