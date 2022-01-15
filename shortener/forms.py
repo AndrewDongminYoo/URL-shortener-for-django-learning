9020b326b77583f14d0f6eeb7abf363c4a0a488d
ydm2790 <ydm2790@gmail.com>
2022-01-15 12:17:19 +0900

    user register form nav

--- shortener/forms.py
+++ shortener/forms.py
@@ -5,8 +5,8 @@ from shortener.models import Users
 
 class RegisterForm(UserCreationForm):
     full_name = forms.CharField(max_length=30, required=False, help_text="Optional.", label="이름")
-    username = forms.CharField(max_length=30, required=False, help_text="Optional.")
-    email = forms.EmailField(max_length=254, help_text="Required. Inform a valid email address.")
+    username = forms.CharField(max_length=30, required=False, help_text="Optional.", label="아이디")
+    email = forms.EmailField(max_length=254, help_text="Required. Inform a valid email address.", label="이메일")
 
     class Meta:
         model = Users
