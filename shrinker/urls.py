6e8a43397a5683ee9fe2011e6f6b7276e77a5a69
ydm2790 <ydm2790@gmail.com>
2022-01-15 12:31:38 +0900

    user login / logout

--- shrinker/urls.py
+++ shrinker/urls.py
@@ -12,14 +12,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from shortener.views import index, get_user, register
+from shortener.views import index, get_user, register, login_view, logout_view
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('__debug__/', include('debug_toolbar.urls')),
+
     path("", index, name="index"),
     path("get_user/<int:user_id>", get_user),
+
     path("register", register, name="register"),
-    path('__debug__/', include('debug_toolbar.urls')),
+    path("login", login_view, name="login"),
+    path("logout", logout_view, name="logout"),
 ]
